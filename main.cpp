d0874ffa7456114c71aa56c92d94d1df24101cfd
thanknoah <78211701+thanknoah@users.noreply.github.com>
2024-08-29 05:08:33 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,16 +11,99 @@
 #include <regex>
 #include <random>
 #include <nlohmann/json.hpp>
-#include "mysql_connection.h"
+#include <mysql_driver.h>
 #include <mysql_connection.h>
 #include <cppconn/statement.h>
 #include <cppconn/resultset.h>
 #include <sstream>
 #include <iomanip>
 #include <cctype>
+#include <mutex>
+#include <condition_variable>
+#include <memory>
 
 using asio::ip::tcp;
 using json = nlohmann::json;
+
+// Essential Global Variables
+asio::io_context ioContext;
+tcp::resolver resolver(ioContext);
+tcp::resolver::results_type endPoints;
+std::shared_ptr<tcp::socket> serverSocket;
+std::mutex mtxPosts;
+std::mutex mtxComments;
+std::string cvPostResp;
+std::string cvCommentsResp;
+std::condition_variable cvPosts; 
+std::condition_variable cvComments;
+
+void activateAIServerListener() {
+    try {
+        // use shared_ptr to manage the lifetime of the AI server
+        serverSocket = std::make_shared<tcp::socket>(ioContext);
+        endPoints = resolver.resolve("127.0.0.1", "62131");
+        auto socket = std::make_shared<tcp::socket>(ioContext);
+        auto sizeBuffer = std::make_shared<std::array<char, sizeof(uint32_t)>>();
+
+        // Connect and establish mutex
+        std::lock_guard<std::mutex> lockPost(mtxPosts);
+        std::lock_guard<std::mutex> lockPost2(mtxComments);
+        asio::connect(*serverSocket, endPoints);
+
+        // Async read the size of the response
+        asio::async_read(*serverSocket, asio::buffer(*sizeBuffer),
+            [sizeBuffer, socket](asio::error_code ec, std::size_t /*len*/) {
+                if (!ec) {
+                    // Assign size of the content to size 
+                    uint32_t size;
+                    std::memcpy(&size, sizeBuffer->data(), sizeof(uint32_t));
+                    size = ntohl(size);
+                    auto dataBuffer = std::make_shared<std::vector<char>>(size);
+
+                    // Asynchronously read the data
+                    asio::async_read(*serverSocket, asio::buffer(*dataBuffer),
+                        [dataBuffer, socket](asio::error_code ec, std::size_t) {
+                            if (!ec) {
+                                std::string resp(dataBuffer->begin(), dataBuffer->end());
+                                cvPostResp = resp;
+                                cvPosts.notify_one();
+                                //cvComments.notify_one();
+                            }
+                            else {
+                                std::cout << "Error reading data: " << ec.message() << std::endl;
+                            }
+                        });
+                }
+                else {
+                    std::cout << "Error reading size: " << ec.message() << std::endl;
+                }
+            });
+
+        ioContext.run();
+    }
+    catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+}
+
+std::string getAPIResponse(tcp::socket& socket) {
+    asio::streambuf resp;
+    asio::read_until(socket, resp, "\r\n\r\n");
+    std::istream resp_stream(&resp); std::stringstream resp_body; std::string header;
+    asio::error_code ec;
+
+    while (std::getline(resp_stream, header) && header != "\r");
+
+    if (resp.size() > 0) {
+        resp_body << &resp;
+    }
+    while (asio::read(socket, resp, asio::transfer_at_least(1), ec)) {
+        resp_body << &resp;
+    }
+
+    std::string resp_body_str = resp_body.str();
+    return resp_body_str;
+}
 
 int returnAmountOfLines(std::string fileName) {
     std::ifstream inputFile(fileName);
@@ -34,7 +117,6 @@ int returnAmountOfLines(std::string fileName) {
 
     return lineCount;
 }
-
 
 std::string generateRequestHeader(std::string path) {
     std::string request = "GET " + path + " HTTP/1.1\r\n";
@@ -86,22 +168,7 @@ void registerRandomUsers(asio::io_context& ioContext, std::string& host, std::st
 
         std::string request = generateRequestHeader(path);
         asio::write(socket, asio::buffer(request));
-        asio::streambuf resp;
-        asio::read_until(socket, resp, "\r\n\r\n");
-
-        std::istream resp_stream(&resp); std::stringstream resp_body; std::string header;
-        asio::error_code ec;
-
-        while (std::getline(resp_stream, header) && header != "\r");
-
-        if (resp.size() > 0) { 
-            resp_body << &resp; 
-        }
-        while (asio::read(socket, resp, asio::transfer_at_least(1), ec)) { 
-            resp_body << &resp; 
-        }
-
-        std::string resp_body_str = resp_body.str();
+        std::string resp_body_str = getAPIResponse(socket);
         std::string resp_body_str_without_quotations = std::regex_replace(resp_body_str, std::regex("\""), "");
         std::size_t found = resp_body_str.find("Exists");
 
@@ -121,35 +188,58 @@ void registerRandomUsers(asio::io_context& ioContext, std::string& host, std::st
 
 }
 
+std::vector<std::string> generateRandomComment(json posts) {
+    try {
+        while (true) {
+            sql::mysql::MySQL_Driver* driver;
+            sql::Connection* con;
+
+            driver = sql::mysql::get_mysql_driver_instance();
+            con = driver->connect("[REDACTED]", "[REDACTED]", "[REDACTED]");
+            con->setSchema("[REDACTED]");
+
+            sql::Statement* stmt = con->createStatement();
+            sql::ResultSet* res = stmt->executeQuery("SELECT id FROM posts ORDER BY id DESC LIMIT 10");
+
+            while (res->next()) {
+                std::cout << "The message is: " << res->getString("_message") << std::endl;
+            }
+
+            // Write to file
+            asio::write(*serverSocket, asio::buffer("generate_title_desc"));
+            std::vector listOfComments;
+
+            // Wait for Response && Converting the resp str into json
+            std::unique_lock<std::mutex> lockPost2(mtxComments);
+            cvComments.wait(lockPost2, [] { return "Ready"; });
+            lockPost2.unlock();
+
+            // Turning it into Json
+            json obj = json::parse(cvCommentsResp);
+            std::vector<std::string> listOfComments;
+        }
+    }
+    catch (std::exception& e) {
+        std::cerr << e.what();
+    }
+}
+
 std::vector<std::string> generateRandomPosts() {
     try {
+        // Token File Length
         std::future<int> tokenLen = std::async(std::launch::async, returnAmountOfLines, "tokens.txt");
         int tokenCount = tokenLen.get();
+
+        // Write to file && socket
         std::ifstream token("tokens.txt");
         std::string token_;
+        asio::write(*serverSocket, asio::buffer("generate_title_desc"));
 
-        asio::io_context ioContext;
-        tcp::resolver resolver(ioContext);
-        tcp::resolver::results_type endPoints = resolver.resolve("127.0.0.1", "62001");
-        tcp::socket socket(ioContext);
-
-        asio::connect(socket, endPoints);
-        asio::write(socket, asio::buffer("generate_title_desc"));
-
-        // Assigning the size of the response to the size variable
-        asio::error_code error;
-        uint32_t size;
-        asio::read(socket, asio::buffer(&size, sizeof(size)));
-        size = ntohl(size);
-
-        // Getting buffer and turning it into resp str
-        std::vector<char> buffer(size);
-        asio::read(socket, asio::buffer(buffer.data(), size));
-        std::string resp(buffer.begin(), buffer.end());
-        resp.erase(std::remove(resp.begin(), resp.end(), '\n'), resp.end());
-
-        // Converting the resp str into json
-        json obj = json::parse(resp);
+        // Wait for Response && Converting the resp str into json
+        std::unique_lock<std::mutex> lockPost(mtxPosts);
+        cvPosts.wait(lockPost, [] { return "Ready"; });
+        lockPost.unlock();
+        json obj = json::parse(cvPostResp);
         std::vector<std::string> listOflinks;
 
         // Looping through json
@@ -167,8 +257,7 @@ std::vector<std::string> generateRandomPosts() {
                 std::getline(token, token_);
             
             std::cout << token_;
-
-            std::string link = "/forum/temp2.php?token=" + token_ + "&title=" + urlEncode(title) + "&desc=" + urlEncode(desc);
+            std::string link = "[REDACTED]";
             listOflinks.push_back(link);
         }
 
@@ -181,48 +270,54 @@ std::vector<std::string> generateRandomPosts() {
 }
 
 std::string generateRandomUser() {
-    std::future<int> namesLength = std::async(std::launch::async, returnAmountOfLines, "names.txt");
-    std::future<int> adjectivesLength = std::async(std::launch::async, returnAmountOfLines, "adjectives.txt");
-    std::future<int> pfpLength = std::async(std::launch::async, returnAmountOfLines, "pfp.txt");
+    try {
+        std::future<int> namesLength = std::async(std::launch::async, returnAmountOfLines, "names.txt");
+        std::future<int> adjectivesLength = std::async(std::launch::async, returnAmountOfLines, "adjectives.txt");
+        std::future<int> pfpLength = std::async(std::launch::async, returnAmountOfLines, "pfp.txt");
 
-    int namesCount = namesLength.get();
-    int adjectivesCount = adjectivesLength.get();
-    int pfpCount = pfpLength.get();
+        int namesCount = namesLength.get();
+        int adjectivesCount = adjectivesLength.get();
+        int pfpCount = pfpLength.get();
 
-    std::ifstream names("names.txt");
-    std::ifstream adjectives("adjectives.txt");
-    std::ifstream pfp("pfp.txt");
-    std::string adjectives_;
-    std::string names_;
-    std::string pfpLink;
-    std::string numbers;
+        std::ifstream names("names.txt");
+        std::ifstream adjectives("adjectives.txt");
+        std::ifstream pfp("pfp.txt");
+        std::string adjectives_;
+        std::string names_;
+        std::string pfpLink;
+        std::string numbers;
 
-    for (int y = 0; y < generateRandomIndex(adjectivesCount - 1); y++)
-        std::getline(adjectives, adjectives_);
+        for (int y = 0; y < generateRandomIndex(adjectivesCount - 1); y++)
+            std::getline(adjectives, adjectives_);
 
-    for (int y = 0; y < generateRandomIndex(namesCount - 1); y++)
-        std::getline(names, names_);
+        for (int y = 0; y < generateRandomIndex(namesCount - 1); y++)
+            std::getline(names, names_);
 
-    for (int y = 0; y < generateRandomIndex(pfpCount - 1); y++)
-        std::getline(pfp, pfpLink);
+        for (int y = 0; y < generateRandomIndex(pfpCount - 1); y++)
+            std::getline(pfp, pfpLink);
 
-    int grammarType = generateRandomIndex(5);
-    int numLen = generateRandomIndex(5);
-    int maxNum = 1;
+        int grammarType = generateRandomIndex(5);
+        int numLen = generateRandomIndex(5);
+        int maxNum = 1;
 
-    if (grammarType == 1 || grammarType == 2) { capitalizeString(adjectives_); capitalizeString(names_); }
-    if (grammarType == 3 || grammarType == 4) { lowercaseString(adjectives_); lowercaseString(names_); }
+        if (grammarType == 1 || grammarType == 2) { capitalizeString(adjectives_); capitalizeString(names_); }
+        if (grammarType == 3 || grammarType == 4) { lowercaseString(adjectives_); lowercaseString(names_); }
 
-    for (int x = 0; x < numLen; x++) { 
-        maxNum = maxNum * 10;
+        for (int x = 0; x < numLen; x++) {
+            maxNum = maxNum * 10;
+        }
+        numbers = std::to_string(generateRandomIndex(maxNum));
+
+        names.close();
+        adjectives.close();
+        pfp.close();
+
+        return "[REDACTED]";
     }
-    numbers = std::to_string(generateRandomIndex(maxNum));
-
-    names.close();
-    adjectives.close();
-    pfp.close();
-
-    return "/forum/temp.php?user=" + adjectives_ + names_ + numbers + "&pfp=" + pfpLink;
+    catch (std::error_code ec) {
+        std::cerr << "Error: " << ec;
+    }
+    
 }
 
 void intializeUserGeneration() {
@@ -244,6 +339,7 @@ void intializeUserGeneration() {
 
 void initializePostGeneration() {
     asio::io_context ioContext;
+    std::cout << "Starting..";
 
     while (true) {
         std::vector<std::string> listOfLinks = generateRandomPosts();
@@ -256,7 +352,7 @@ void initializePostGeneration() {
 
             std::string request = generateRequestHeader(it);
             asio::write(socket, asio::buffer(request));
-            // std::cout << "Generated new post at https://thenoahdevs.com/" + it + "\n";
+            std::cout << "Generated new post at https://thenoahdevs.com/" + it + "\n";
         }
         std::this_thread::sleep_for(std::chrono::seconds(15));
     }
@@ -282,9 +378,9 @@ int main() {
     std::string host = "thenoahdevs.com";
     std::vector<std::thread> threads;
     
-    //for (int x = 0; x < 1; x++) { threads.emplace_back(initializePostGeneration)); }
-    initializePostGeneration();
-    //for (auto& t : threads) {
-       // t.join();
-   // }
+    for (int x = 0; x < 1; x++) { threads.emplace_back(activateAIServerListener); }
+    for (int x = 0; x < 1; x++) { threads.emplace_back(initializePostGeneration); }
+    for (auto& t : threads) {
+        t.join();
+    }
 }
